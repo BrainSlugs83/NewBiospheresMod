0ee26275672c5c04553e93308964490c0429b1f6
BrainSlugs83@gmail.com <BrainSlugs83@gmail.com>
2014-10-09 20:44:01 -0700

    Updated the sources with proper license blurb (see: http://www.wtfpl.net/faq/ )

--- src/main/java/newBiospheresMod/Models/NoiseChunk.java
+++ src/main/java/newBiospheresMod/Models/NoiseChunk.java
@@ -1,3 +1,9 @@
+/*
+ * This is free software. It comes without any warranty, to the extent permitted by applicable law. You can redistribute
+ * it and/or modify it under the terms of the Do What The Fuck You Want To Public License, Version 2, as published by
+ * Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+
 package newBiospheresMod.Models;
 
 import net.minecraft.world.World;
