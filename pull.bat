c3e4a0eec46b5530352f44cc4c9e2e99cbad70f6
Michael Jensen <BrainSlugs83@gmail.com>
2015-06-07 22:02:50 -0700

    Updated pull.bat.

--- pull.bat
+++ pull.bat
@@ -1,1 +1,18 @@
-@bzr pull https://github.com/BrainSlugs83/NewBiospheresMod.git/,branch=master 
+@echo off
+SETLOCAL
+
+echo ARE YOU SURE YOU WANT TO GET THE LATEST CODEBASE?
+echo (ALL PENDING CHANGES IN THIS BRANCH WILL BE LOST!)
+set /p ans=^>
+if /i NOT "%ans:~0,1%" == "y" EXIT /B 1
+
+:: bzr pull https://github.com/BrainSlugs83/NewBiospheresMod.git/,branch=master 
+
+echo.
+echo ===== Pulling =====
+git pull
+
+echo.
+echo ===== Resetting to Latest =====
+git reset --hard HEAD
+
